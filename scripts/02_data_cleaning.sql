e955a8a78c6eb8c2aabc771ea17a4a6966bd1f68
Chrisislearning <70579236+Chrisislearning@users.noreply.github.com>
2025-08-23 13:25:39 +0200

    Update 02_data_cleaning.sql

--- scripts/02_data_cleaning.sql
+++ scripts/02_data_cleaning.sql
@@ -1,7 +1,7 @@
 
 /*========================================================
 Set of queries aimed at detecting errors in
-selected tables. Types of errors looked for:
+tables. Types of errors looked for:
 Nulls - with "is null";
 Inconsistent  low cardinalities - with "select distinct";
 Negative values - with sign() function;
@@ -16,53 +16,52 @@ concatenation to create the list of column names.
 ========================================================*/
 
 
-/*============================================
-			     DimCustomer
-============================================*/
+/*========================================================
+			         DimCustomer
+========================================================*/
 
 --create the list of columns to search through
 --and check them for nulls
 select
 	string_agg(COLUMN_NAME + ' is null', ' or ')
 from INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'DimCustomer'
-and IS_NULLABLE = 'YES'
+where TABLE_SCHEMA = 'eda'
+and TABLE_NAME = 'DimCustomer'
+and IS_NULLABLE = 'YES';
 
 --copy the generated string and use it in query below
 --choose columns important for analysis
 select
-	CustomerKey
-	,BirthDate
+	 BirthDate
 	,MaritalStatus
-	,Gender
 	,YearlyIncome
 	,TotalChildren
 	,NumberChildrenAtHome
 	,HouseOwnerFlag
 	,NumberCarsOwned
-from DimCustomer
-where 	
-       BirthDate            is null
-	or MaritalStatus        is null
-	or Gender               is null
-	or YearlyIncome         is null
-	or TotalChildren        is null
-	or NumberChildrenAtHome is null
-	or HouseOwnerFlag       is null
-	or NumberCarsOwned      is null;
-
+	,DateFirstPurchase
+from eda.DimCustomer
+where 
+	   BirthDate			is null 
+	or MaritalStatus		is null 
+	or YearlyIncome			is null 
+	or TotalChildren		is null 
+	or NumberChildrenAtHome is null 
+	or HouseOwnerFlag		is null 
+	or NumberCarsOwned		is null 
+	or DateFirstPurchase	is null;
+	   
 --check for errors in low cardinality columns
-select distinct MaritalStatus from DimCustomer;
-select distinct Gender from DimCustomer;
+select distinct MaritalStatus from eda.DimCustomer;
 
 --check for negative numbers
 select
-	YearlyIncome
+	 YearlyIncome
 	,TotalChildren
 	,NumberChildrenAtHome
 	,HouseOwnerFlag
 	,NumberCarsOwned
-from DimCustomer
+from eda.DimCustomer
 where 	
 	   sign(YearlyIncome)         < 0
 	or sign(TotalChildren)        < 0
@@ -72,232 +71,209 @@ where
 
 --look for incompatible dates
 select
-	BirthDate,
-	DateFirstPurchase
-from DimCustomer
+	 BirthDate
+	,DateFirstPurchase
+from eda.DimCustomer
 where BirthDate >= DateFirstPurchase;
 
 
-/*============================================
-         	   ProspectiveBuyer
-============================================*/
+/*========================================================
+			         DimDate
+========================================================*/
 
---choose columns important for analysis
---and check for nulls
+select distinct EnglishDayNameOfWeek from eda.DimDate;
+select distinct EnglishMonthName from eda.DimDate;
+
+
+/*========================================================
+         			 DimGeography
+========================================================*/
+
+--nulls
 select
-	ProspectiveBuyerKey
-	,BirthDate
-	,MaritalStatus
-	,Gender
-	,YearlyIncome
-	,TotalChildren
-	,NumberChildrenAtHome
-	,HouseOwnerFlag
-	,NumberCarsOwned
-	,StateProvinceCode
-from ProspectiveBuyer
+	 City 
+	,StateProvinceCode 
+	,StateProvinceName 
+	,CountryRegionCode 
+	,EnglishCountryRegionName 
+	,SalesTerritoryKey
+	from eda.DimGeography
 where
-	   ProspectiveBuyerKey  is null
-	or BirthDate            is null
-	or MaritalStatus        is null
-	or Gender               is null
-	or YearlyIncome         is null
-	or TotalChildren        is null
-	or NumberChildrenAtHome is null
-	or HouseOwnerFlag       is null
-	or NumberCarsOwned      is null
-	or StateProvinceCode    is null;
+	   City						 is null
+	or StateProvinceCode         is null
+	or StateProvinceName		 is null
+	or CountryRegionCode		 is null
+	or EnglishCountryRegionName  is null
+	or SalesTerritoryKey		 is null;
 
 --check for errors in low cardinality columns
-select distinct MaritalStatus from ProspectiveBuyer;
-select distinct Gender from ProspectiveBuyer;
-select distinct StateProvinceCode from ProspectiveBuyer
-order by StateProvinceCode;
-
---check for negative numbers
-select
-	YearlyIncome
-	,TotalChildren
-	,NumberChildrenAtHome
-	,HouseOwnerFlag
-	,NumberCarsOwned
-from ProspectiveBuyer
-where 
-	   sign(YearlyIncome)          < 0
-	or sign(TotalChildren)         < 0
-	or sign(NumberChildrenAtHome)  < 0
-	or sign(HouseOwnerFlag)        < 0
-	or sign(NumberCarsOwned)       < 0
+select distinct StateProvinceName from DimGeography;
+select distinct CountryRegionCode from DimGeography;
+select distinct EnglishCountryRegionName from DimGeography;
 
 
-/*============================================
-           		FactInternetSales
-============================================*/
+/*========================================================
+           			FactInternetSales
+========================================================*/
 
 --choose columns important for analysis,
 --don't include redundant data
 select
-	ProductKey
+	 ProductKey
+	,OrderDateKey
 	,CustomerKey
-	,SalesOrderNumber
-	,SalesOrderLineNumber
+	,CurrencyKey
+	,SalesTerritoryKey
+	,SalesSurrogateKey
 	,OrderQuantity
 	,UnitPrice
-	,SalesAmount
-from FactInternetSales;
---selected columns are all not nullable
-
-
-/*============================================
-         	  	FactResellerSales
-============================================*/
-
---check for nulls
-select 
-	ProductKey
-	,SalesOrderNumber
-	,SalesOrderLineNumber
-	,OrderQuantity
-	,UnitPrice
-	,ExtendedAmount
 	,UnitPriceDiscountPct
 	,DiscountAmount
 	,ProductStandardCost
 	,TotalProductCost
 	,SalesAmount
-from FactResellerSales
-where 
-       OrderQuantity        is null 
-	or UnitPrice            is null 
-	or ExtendedAmount       is null 
-	or UnitPriceDiscountPct is null 
-	or DiscountAmount       is null 
-	or ProductStandardCost  is null 
-	or TotalProductCost     is null 
-	or SalesAmount          is null;
+	,OrderDate
+from eda.FactInternetSales
+where
+	   ProductKey           is null
+	or OrderDateKey         is null
+	or CustomerKey          is null
+	or CurrencyKey          is null
+	or SalesTerritoryKey    is null
+	or SalesSurrogateKey    is null
+	or OrderQuantity        is null
+	or UnitPrice            is null
+	or UnitPriceDiscountPct is null
+	or DiscountAmount       is null
+	or ProductStandardCost  is null
+	or TotalProductCost     is null
+	or SalesAmount          is null
+	or OrderDate            is null;
 
---check strings for trailing spaces
+/*========================================================
+         	  	FactResellerSales
+========================================================*/
+
+--check for nulls
 select
-	SalesOrderNumber
-from FactResellerSales
-where len(SalesOrderNumber) != len(trim(SalesOrderNumber));
+	string_agg(COLUMN_NAME,',')
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_SCHEMA = 'eda'
+and TABLE_NAME = 'FactResellerSales'
+and IS_NULLABLE = 'YES';
+
+select 
+	  ResellerGeographyKey
+	 ,SalesSurrogateKey
+	 ,OrderQuantity
+	 ,UnitPrice
+	 ,ExtendedAmount
+	 ,UnitPriceDiscountPct
+	 ,DiscountAmount
+	 ,ProductStandardCost
+	 ,TotalProductCost
+	 ,SalesAmount
+	 ,TaxAmt
+	 ,Freight
+	 ,OrderDate
+	 ,ProductLine
+	 ,ResellerAnnualSales
+	 ,ResellerAnnualRevenue
+from eda.FactResellerSales
+where 
+	    ResellerGeographyKey	is null
+	 or SalesSurrogateKey		is null
+	 or OrderQuantity			is null
+	 or UnitPrice				is null
+	 or ExtendedAmount			is null
+	 or UnitPriceDiscountPct	is null
+	 or DiscountAmount			is null
+	 or ProductStandardCost		is null
+	 or TotalProductCost		is null
+	 or SalesAmount				is null
+	 or TaxAmt					is null
+	 or Freight					is null
+	 or OrderDate				is null
+	 or ProductLine				is null
+	 or ResellerAnnualSales		is null
+	 or ResellerAnnualRevenue	is null;
+
+--check cardinality
+select distinct ResellerBusinessType from eda.FactResellerSales;
+select distinct ProductLine from eda.FactResellerSales;
 
 --check for negative numbers
---how to check many columns:
---use the resulting string in WHERE clause
+--get list of columns
 select
 	string_agg('sign(' + COLUMN_NAME + ')' + ' < 0', ' or ')
 from INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'FactResellerSales'
-and NUMERIC_PRECISION is not null   --this filters out non-numbers
-and COLUMN_NAME in (                --copied columns from earlier query
-	 'ProductKey'
-	,'SalesOrderNumber'
-	,'SalesOrderLineNumber'
-	,'OrderQuantity'
-	,'UnitPrice'
-	,'ExtendedAmount'
-	,'UnitPriceDiscountPct'
-	,'DiscountAmount'
-	,'ProductStandardCost'
-	,'TotalProductCost'
-	,'SalesAmount'
-);
+where TABLE_SCHEMA = 'eda'
+and TABLE_NAME = 'FactResellerSales'
+and NUMERIC_PRECISION is not null;   --this filters out non-numbers
 
 select 
-	ProductKey
-	,SalesOrderNumber
-	,SalesOrderLineNumber
-	,OrderQuantity
-	,UnitPrice
-	,ExtendedAmount
-	,UnitPriceDiscountPct
-	,DiscountAmount
-	,ProductStandardCost
-	,TotalProductCost
-	,SalesAmount
-from FactResellerSales
+	 *
+from eda.FactResellerSales
 where --copy and format the result from previous query
-	   sign(ProductKey) < 0 
-	or sign(SalesOrderLineNumber) < 0 
-	or sign(OrderQuantity) < 0 
-	or sign(UnitPrice) < 0 
-	or sign(ExtendedAmount) < 0 
-	or sign(UnitPriceDiscountPct) < 0 
-	or sign(DiscountAmount) < 0 
-	or sign(ProductStandardCost) < 0 
-	or sign(TotalProductCost) < 0 
-	or sign(SalesAmount) < 0;
+	   sign(ProductKey)			   < 0 
+	or sign(OrderDateKey)		   < 0 
+	or sign(ResellerKey)		   < 0 
+	or sign(ResellerGeographyKey)  < 0 
+	or sign(CurrencyKey)		   < 0 
+	or sign(SalesTerritoryKey)     < 0 
+	or sign(OrderQuantity)		   < 0 
+	or sign(UnitPrice)			   < 0 
+	or sign(ExtendedAmount)		   < 0 
+	or sign(UnitPriceDiscountPct)  < 0 
+	or sign(DiscountAmount)		   < 0 
+	or sign(ProductStandardCost)   < 0 
+	or sign(TotalProductCost)	   < 0 
+	or sign(SalesAmount)		   < 0 
+	or sign(TaxAmt)				   < 0 
+	or sign(Freight)			   < 0 
+	or sign(ResellerAnnualSales)   < 0 
+	or sign(ResellerAnnualRevenue) < 0;
 
-
-/*============================================
+/*========================================================
          	    	DimProduct
-============================================*/
+========================================================*/
 
 --check for nulls
 select
 	string_agg(COLUMN_NAME + ' is null', ' or ')
 from INFORMATION_SCHEMA.COLUMNS
-where TABLE_NAME = 'DimProduct'
-and IS_NULLABLE = 'YES'
-and COLUMN_NAME in (
-	 'ProductKey'
-	,'ProductSubcategoryKey'
-	,'EnglishProductName'
-	,'StandardCost'
-	,'FinishedGoodsFlag'
-	,'ListPrice'
-	,'DealerPrice'
-	,'StartDate'
-	,'EndDate'
-	,'Status'
-);
+where TABLE_SCHEMA = 'eda'
+and TABLE_NAME = 'DimProduct'
+and IS_NULLABLE = 'YES';
 
 select  
-	ProductKey
-	,ProductSubcategoryKey
-	,EnglishProductName
-	,StandardCost
-	,FinishedGoodsFlag
-	,ListPrice
-	,DealerPrice
-	,StartDate
-	,EndDate
-	,Status
-from DimProduct
+	*
+from 
+	(select * from eda.DimProduct  --filter out parts
+	where FinishedGoodsFlag = 1)t
 where
-	   ProductSubcategoryKey is null 
-	or StandardCost		     is null 
-	or ListPrice			 is null 
-	or DealerPrice			 is null 
-	or StartDate			 is null 
-	or EndDate				 is null 
-	or Status				 is null;
-
+	   ProductAlternateKey			 is null 
+	or EnglishProductCategoryName	 is null 
+	or EnglishProductSubcategoryName is null 
+	or StandardCost					 is null 
+	or ListPrice					 is null 
+	or ProductLine					 is null 
+	or DealerPrice					 is null 
+	or ModelName					 is null
+	or StartDate					 is null;
+--errors found in 4 columns, consult report.
+	
 --check cardinalities
-select distinct FinishedGoodsFlag from DimProduct;
+select distinct EnglishProductCategoryName from eda.DimProduct;
+select distinct EnglishProductSubcategoryName from eda.DimProduct;
+select distinct ProductLine from eda.DimProduct;
 
---look at only finished products, not parts
-select  
-	ProductKey
-	,ProductSubcategoryKey
-	,EnglishProductName
-	,StandardCost
-	,FinishedGoodsFlag
-	,ListPrice
-	,DealerPrice
-	,StartDate
-	,EndDate
-	,Status
-from DimProduct
-where FinishedGoodsFlag = 1;
---errors found in 3 columns, consult report.
 
 --check rows with errors for possible duplicates
 --with corrected data
 select
-	ProductKey
-	,ProductSubcategoryKey
+	 ProductKey
+	,ProductAlternateKey
 	,EnglishProductName
 	,StandardCost
 	,FinishedGoodsFlag
@@ -305,39 +281,35 @@ select
 	,DealerPrice
 	,StartDate
 	,EndDate
-	,Status
-from DimProduct
+	,ProductionStatus
+from eda.DimProduct
 where ProductKey in (210, 211)
-   or EnglishProductName in ('HL Road Frame - Black, 58', 'HL Road Frame - Red, 58')
+   or EnglishProductName in ('HL Road Frame - Black, 58', 'HL Road Frame - Red, 58');
+
+select
+	*
+from eda.DimProduct
+where ProductLine is null
+and FinishedGoodsFlag = 1;
 
 --check dates
 select  
-	ProductKey
-	,ProductSubcategoryKey
-	,EnglishProductName
-	,StartDate
-	,EndDate
-	,Status
-from DimProduct
-where StartDate >= EndDate
+	*
+from eda.DimProduct
+where StartDate >= EndDate;
 --200 products have StartDate later than EndDate.
 
 --check prices
 select  
-	ProductKey
-	,EnglishProductName
-	,StandardCost
-	,FinishedGoodsFlag
-	,ListPrice
-	,DealerPrice
-	,Status
-from DimProduct
-where StandardCost >= DealerPrice
+	*
+from eda.DimProduct
+where StandardCost >= DealerPrice;
 --note: 114 products sold below the cost. Might not be an error.
 
 --check for duplicates
 select  
-	ProductSubcategoryKey
+	 EnglishProductCategoryName
+	,EnglishProductSubcategoryName
 	,EnglishProductName
 	,StandardCost
 	,FinishedGoodsFlag
@@ -345,11 +317,12 @@ select
 	,DealerPrice
 	,StartDate
 	,EndDate
-	,Status
+	,ProductionStatus
 	,count(*)
-from DimProduct
+from eda.DimProduct
 group by 
-	ProductSubcategoryKey
+	 EnglishProductCategoryName
+	,EnglishProductSubcategoryName
 	,EnglishProductName
 	,StandardCost
 	,FinishedGoodsFlag
@@ -357,22 +330,23 @@ group by
 	,DealerPrice
 	,StartDate
 	,EndDate
-	,Status
-having count(*) > 1
-
-/*============================================
-        	DimProductSubcategory
-			 DimProductCategory
-============================================*/
-
---check for missing names
-select 
-	EnglishProductSubcategoryName 
-from DimProductSubcategory
-where EnglishProductSubcategoryName is null;
+	,ProductionStatus
+having count(*) > 1;
 
 
-select
-	EnglishProductCategoryName
-from DimProductCategory
-where EnglishProductCategoryName is null;
+/*========================================================
+         	    	FactCurrencyRate
+========================================================*/
+
+select * from eda.FactCurrencyRate
+where AverageRate <= 0
+or EndOfDayRate <= 0;
+
+select distinct CurrencyName from eda.FactCurrencyRate;
+
+/*========================================================
+         	    	FactInternetSalesReason
+========================================================*/
+
+select distinct SalesReasonName from eda.FactInternetSalesReason;
+select distinct SalesReasonReasonType from eda.FactInternetSalesReason;
