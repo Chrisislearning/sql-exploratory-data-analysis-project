e1f08cdbe0d5e3130f53bb57b02d5cd37b84c884
Chrisislearning <70579236+Chrisislearning@users.noreply.github.com>
2025-08-26 12:40:42 +0200

    Update 04_basic_aggregations.sql

--- scripts/04_basic_aggregations.sql
+++ scripts/04_basic_aggregations.sql
@@ -1,16 +1,16 @@
 /*===================================================
-                Basic aggregations
+				Basic aggregations
 ===================================================*/
 
 
 --summary of companys "big numbers" - highest level of aggregation
-select 'Total sales' as MeasureName, sum(SalesAmount) as MeasureValue from eda.vFactSalesCombined
+select 'Total sales' as MeasureName, round(sum(SalesAmount), 2) as MeasureValue from eda.vFactSalesCombined
 union all
-select 'Total profit', sum(UnitPrice - ProductStandardCost) from eda.vFactSalesCombined
+select 'Total profit', round(sum(UnitPrice - ProductStandardCost), 2) from eda.vFactSalesCombined
 union all
-select 'Average item price', avg(UnitPrice) from eda.vFactSalesCombined
+select 'Average item price', round(avg(UnitPrice), 2) from eda.vFactSalesCombined
 union all
-select 'Average profit per item', avg(UnitPrice - ProductStandardCost) from eda.vFactSalesCombined
+select 'Average profit per item', round(avg(UnitPrice - ProductStandardCost), 2) from eda.vFactSalesCombined
 union all
 select 'Total orders', count(SalesSurrogateKey) from eda.vFactSalesCombined
 union all
@@ -21,28 +21,28 @@ union all
 select 'Total website customers', count(distinct CustomerKey) from eda.FactInternetSales;
 
 
---total customers by country
+--total customers per country
 --based on internet sales
-select 
-	 count(d.CustomerKey) as CustomerCount
-	,g.EnglishCountryRegionName as CountryName
+select
+	 g.EnglishCountryRegionName as CountryName
+	,count(d.CustomerKey) as Customers
 from eda.DimCustomer as d 
 join eda.DimGeography as g
 on d.GeographyKey = g.GeographyKey
 group by g.EnglishCountryRegionName
 order by count(d.CustomerKey) desc
 
---total sales by country
-select 
-	 round(sum(s.SalesAmount), 2) as TotalSales
-	,t.SalesTerritoryCountry as Country
+--total sales per country
+select
+	 t.SalesTerritoryCountry as Country
+	,round(sum(s.SalesAmount), 2) as TotalSales
 from eda.vFactSalesCombined as s
 join eda.DimSalesTerritory as t
 on s.SalesTerritoryKey = t.SalesTerritoryKey
 group by t.SalesTerritoryCountry
 order by round(sum(s.SalesAmount), 2) desc
 
---total products by category
+--total products per category
 select
 	 EnglishProductCategoryName as Category
 	,count(EnglishProductCategoryName) as Products
@@ -51,17 +51,17 @@ where FinishedGoodsFlag = 1
 group by EnglishProductCategoryName
 order by count(EnglishProductCategoryName) desc
 
---total sales by category
-select 
-	 round(sum(s.SalesAmount), 2) as TotalSales
-	,p.EnglishProductCategoryName as Category
+--total sales per category
+select
+	 p.EnglishProductCategoryName as Category
+	,round(sum(s.SalesAmount), 2) as TotalSales
 from eda.vFactSalesCombined as s
 join eda.DimProduct as p
 on s.ProductKey = p.ProductKey
 group by p.EnglishProductCategoryName
 order by round(sum(s.SalesAmount), 2) desc
 
---average profit and margin by category
+--average profit and margin per category
 select
 	 p.EnglishProductCategoryName as Category
 	 ,round(avg(s.SalesAmount - s.TotalProductCost), 2) as ProfitDollars
@@ -71,12 +71,21 @@ join eda.DimProduct as p
 on s.ProductKey = p.ProductKey
 group by p.EnglishProductCategoryName
 
---items sold by country
+--items sold per country
 select 
-	 sum(s.OrderQuantity) as ItemsSold
-	,t.SalesTerritoryCountry
+	 t.SalesTerritoryCountry as Country
+	,sum(s.OrderQuantity) as ItemsSold
 from eda.vFactSalesCombined as s
 join eda.DimSalesTerritory as t
 on s.SalesTerritoryKey = t.SalesTerritoryKey
 group by t.SalesTerritoryCountry
 order by sum(s.OrderQuantity) desc
+
+--orders per customer
+select 
+	CustomerKey
+	,count(SalesSurrogateKey) PlacedOrders
+	--no need for OrderQuantity as they all = 1
+from eda.FactInternetSales
+group by CustomerKey
+order by count(SalesSurrogateKey) desc
