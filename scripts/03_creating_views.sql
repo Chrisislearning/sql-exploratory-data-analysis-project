ab4f0adc74616bd3f3d226cb9b9dd0cb66203e12
Chrisislearning <70579236+Chrisislearning@users.noreply.github.com>
2025-08-26 23:59:18 +0200

    Update 03_creating_views.sql

--- scripts/03_creating_views.sql
+++ scripts/03_creating_views.sql
@@ -1,12 +1,14 @@
+
+
 /*============================================
-Creating views for ease of aggregating
+     Creating views for ease of aggregating
 ============================================*/
 
-/*======================================
-FactInternetSales and FactResellerSales
-Combined into vFactSalesCombined
-======================================*/
 
+/*======================================
+ FactInternetSales and FactResellerSales
+ Combined into vFactSalesCombined
+======================================*/
 create view eda.vFactSalesCombined as
 select
 	 ProductKey
@@ -41,3 +43,35 @@ select
 	,SalesAmount
 	,OrderDate
 from eda.FactResellerSales
+
+/*======================================
+        customers assigned to 
+        income and spending groups
+======================================*/
+create view eda.vCustomersSpendings as
+with CTE_Sales as (
+    select
+         s.CustomerKey
+        ,sum(s.SalesAmount) as TotalSpending
+        ,case
+            when sum(s.SalesAmount) < 200 then 'Low'
+            when sum(s.SalesAmount) < 3000 then 'Medium'
+            else 'High'
+         end as SpendingRange
+        ,count(s.SalesSurrogateKey) as NoOfOrders
+    from eda.FactInternetSales as s
+    group by s.CustomerKey
+    )
+select
+     s.CustomerKey
+    ,case
+       when c.YearlyIncome < 40000 then 'Low'
+       when c.YearlyIncome < 70000 then 'Medium'
+       else 'High'
+     end as IncomeRange
+    ,s.SpendingRange
+    ,s.TotalSpending
+    ,s.NoOfOrders
+from CTE_Sales as s
+join eda.DimCustomer as c
+on s.CustomerKey = c.CustomerKey;
